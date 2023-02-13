e995d61b0fd015c8d40b37fcc96ff621eaa08554
Ensers.io <124688509+ensers@users.noreply.github.com>
2023-02-13 12:51:20 +0000

    home route and started on query route

--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app=Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return "<p> Hello</p>"
+
+@app.route("/querry", methods=['GET','POST'])
+def querry():
+    if request.method== 'POST':
+        pass
+    else:
+        return "<p> Paste your querry </p>"
