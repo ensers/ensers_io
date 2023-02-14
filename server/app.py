8e793fd1ed0efefb74a417421360fbb181881096
Ensers.io <124688509+ensers@users.noreply.github.com>
2023-02-14 07:52:27 +0000

    added a functional process_docs route

--- server/app.py
+++ server/app.py
@@ -1,10 +1,23 @@
+
+import sys
+sys.path.append('../semantic_search')
+import Load_docs
+
 from flask import Flask, request
+from semantic_search  import Load_docs
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
     return "<p> Hello</p>"
+
+@app.route("/process_docs")
+def process_docs():
+    document=Load_docs.Process_Docs(doc_dir='../content')
+    document.get_docs()
+    res=document.process_docs()
+    return res
 
 @app.route("/querry", methods=['GET','POST'])
 def querry():
