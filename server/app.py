68f8fe77c8edd986d771b738a37f91ec31f1341f
Ensers.io <124688509+ensers@users.noreply.github.com>
2023-02-15 09:11:47 +0000

    write documents to vector db

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../semantic_search')
 import Load_docs
 
-from flask import Flask, request
+from flask import Flask, request,json
 from semantic_search  import Load_docs , semantic_gpu
 
 
@@ -15,12 +15,27 @@ def home():
 
 @app.route("/process_docs")
 def process_docs():
-    document=Load_docs.Process_Docs(doc_dir='../content')
-    document.get_docs()
-    res=document.process_docs()
-    return res
+    try:
+        document=Load_docs.Process_Docs(doc_dir='../content')
+        document.get_docs()
+        document.process_docs()
+        document.write_docs()
+        res="Write successfull"
+        response = app.response_class(
+            response=json.dumps(res),
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception as e:
+        response = app.response_class(
+            response=json.dumps(e),
+            status=200,
+            mimetype='application/json'
+            )
+    return response
 
 @app.route("/querry", methods=['GET','POST'])
+
 def querry():
     if request.method== 'POST':
         query=request.args.get('key', '')
@@ -29,4 +44,4 @@ def querry():
         #     params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 3}})
         return request
     else:
-        return request.to_json()
+        return jsonify(request)
